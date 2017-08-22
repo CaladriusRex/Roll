81cdaeb340b66bca5307915b0bb4d51cc168df81
IvanSimao <ivanezequielsimao@gmail.com>
2017-08-22 12:17:04 -0300

    Agregue movimiento y edite el git ignore.

---
+++ 2D Anim/Assets/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    Animator anim;
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            anim.SetBool("Walking Left", true);
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+            anim.SetBool("Walking Left", false);
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            anim.SetBool("Walking Right", true);
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+            anim.SetBool("Walking Right", false);
+    }
+}
